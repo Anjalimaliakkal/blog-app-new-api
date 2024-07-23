8d02801a70453bf8e342f55d6f9c8ca11e8fe687
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-07-23 21:21:53 +0530

    signup completed

--- app.js
+++ app.js
@@ -3,12 +3,40 @@ const Mongoose = require("mongoose")
 const Bcrypt = require("bcrypt")
 const Cors = require("cors")
 const jwt = require("jsonwebtoken")
+const userModel = require("./models/users")
 
 let app = Express()
 
-app.get("/", (req, res) => {
-    res.send("hello")
+app.use(Express.json())
+app.use(Cors())
+
+Mongoose.connect("mongodb+srv://anjali2003:anjali2003@cluster0.wy6js.mongodb.net/blogappnewdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/signup", async (req, res) => {
+    let input = req.body
+    let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
+    console.log(hashedPassword)
+    req.body.password = hashedPassword
+
+    userModel.find({ email: req.body.email }).then(
+        (items) => {
+    
+    if (items.length > 0) {
+
+        res.json({ "status": "email id already exist" })
+
+    } else {
+
+        let result = new userModel(input)
+        result.save()
+        res.json({ "status": "success" })
+    }
+
+
+    //res.send(data)
 })
-app.listen(3030,() => {
+})
+
+app.listen(8080, () => {
     console.log("server started")
 })
