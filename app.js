a19eaaf0641e925e2271bec755c06ef5c5b7c592
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-07-25 21:51:40 +0530

    viewall completed

--- app.js
+++ app.js
@@ -32,6 +32,29 @@ app.post("/create", async (req, res) => {
 
 })
 
+//view all post
+
+app.post("/viewall",(req,res)=>{
+
+    let token=req.headers.token
+    jwt.verify(token,"blogApp",(error,decoded)=>{
+        if (decoded && decoded.email) {
+            
+            postModel.find().then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":"error"})
+                }
+            )
+        } else {
+            res.json({"status":"invalid Authentication"})
+        }
+    })
+})
+
 //sign in
 app.post("/signin", async (req, res) => {
 
